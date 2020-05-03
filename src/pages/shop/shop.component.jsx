177faf323925bf8b0a770af63280fab7ff2c88d8
Zain Hatim <zain.hatim@gmail.com>
2020-05-03 01:57:26 -0400

    withSpinner component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 import Collection from '../collection/collection.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-const ShopPage = ({match}) => (
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionsWithSpinner = WithSpinner(Collection);
+
+class ShopPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true
+    };
+
+    setTimeout(() => this.setState({
+      loading: false
+    }), 1000);  
+  }
+  
+  render() { 
+    const {loading} = this.state;
+    const {match} = this.props;
+    return (
   <div className='shop-page'>
-    <Route exact path={`${match.path }`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={Collection} />
+    <Route exact path={`${match.path }`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
+    <Route path={`${match.path}/:collectionId`} redner={(props) => <CollectionsWithSpinner isLoading={loading} {...props} />} />
   </div>
-);
+  )}
+}
 
 export default ShopPage;
